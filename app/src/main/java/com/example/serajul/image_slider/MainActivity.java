a049380665fda3f28c011525da80831abee229ac
SerajulIslam <serajulislam666@gmail.com>
2019-03-19 22:49:48 +0600

    commit

---
+++ app/src/main/java/com/example/serajul/image_slider/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.serajul.image_slider;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = findViewById(R.id.viewPage);
+
+        ImageAdapter adapter = new ImageAdapter(this);
+        viewPager.setAdapter(adapter);
+    }
+}
